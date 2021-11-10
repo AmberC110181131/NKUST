236a5f1bf90ecace67aea060c50d905d2e5bab5e
AmberC110181131 <93572220+AmberC110181131@users.noreply.github.com>
2021-11-10 20:54:05 +0800

    Add files via upload

---
+++ NKUST1109/BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+    public static void main( String[] args) {
+        int weight=48;
+        float high=1.58f;
+        float bmi;
+        bmi=weight / (high * high);
+        System.out.println("BMI=" + bmi);
+    }
+}
